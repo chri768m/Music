5098e0860ee5dbc3b85282406679b585a24a9555
chri768m <Stormaxe@live.dk>
2021-03-24 09:55:38 +0100

    da music
    is da rythm

---
+++ Music/Music/Song.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Music
+{
+    public class Song
+    {
+        private string title_;
+        private string artist_;
+        private int duration_;
+        private int yearofpublication_;
+
+
+
+
+        public String Title
+        {
+            get => title_;
+            set { title_ = value; }
+        }
+        public String Artist
+        {
+            get => artist_;
+            set { artist_ = value; }
+        }
+        public int Duration
+        {
+            get => duration_;
+            set { duration_ = value; }
+        }
+        public int YearOfPublication
+        {
+            get => yearofpublication_;
+            set { yearofpublication_ = value; }
+        }
+
+
+        public Song()
+        {
+
+        }
+
+    }
+}
