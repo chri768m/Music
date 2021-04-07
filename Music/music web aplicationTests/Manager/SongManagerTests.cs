fd44ff6d63458579d72b72132523e2c55c226e80
Stephan2620 <43337048+Stephan2620@users.noreply.github.com>
2021-04-07 13:27:19 +0200

    unitest af manager

---
+++ Music/music web aplicationTests/Manager/SongManagerTests.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using music_web_aplication.Manager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace music_web_aplication.Manager.Tests
+{
+    [TestClass()]
+    public class SongManagerTests
+    {
+        [TestMethod()]
+        public void getAllTest()
+        {
+            SongManager manager = new SongManager();
+            List =  manager.getAll();
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetByIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void UpdateTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
