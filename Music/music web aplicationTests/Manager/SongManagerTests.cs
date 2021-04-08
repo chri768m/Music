0a562d57b0e1276ba78741cc15200f3813974318
MikkelYtting <Mytting1996@gmail.com>
2021-04-08 10:22:33 +0200

    dunno

---
+++ Music/music web aplicationTests/Manager/SongManagerTests.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using music_web_aplication.Manager;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace music_web_aplication.Manager.Tests
+{
+    [TestClass()]
+    public class SongManagerTests
+    {
+        [TestMethod()]
+        public void AddTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
